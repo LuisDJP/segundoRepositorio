59509f82a63c6cc112bcb02d5489b4836223d912
LuisDJP <luis.deudasjudi@gmail.com>
2024-10-23 12:22:26 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,265 @@
+<html>
+
+<head>
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-P94F9734');</script>
+    <!-- End Google Tag Manager -->
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="robots" content="max-image-preview:large">
+    <title>Página de prueba Deudas</title>
+
+    <link href="https://www.cvmabogados.com.pe/wp-content/themes/RUN4QCZ6C66jHxHAGqds/images/isotipo.svg" rel="icon">
+
+    <!-- Enlace a Google Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://www.cvmabogados.com.pe/wp-content/themes/RUN4QCZ6C66jHxHAGqds/style.css?v=1729134459" type="text/css">
+    <link rel="stylesheet" href="https://www.cvmabogados.com.pe/wp-content/themes/RUN4QCZ6C66jHxHAGqds/css/slick.css?v=1729134459" type="text/css">
+
+    <script type="text/javascript" async src="https://www.googletagmanager.com/gtag/js?id=AW-11300207138&amp;l=dataLayer&amp;cx=c"></script>
+    <script async src="https://www.googletagmanager.com/gtm.js?id=GTM-P94F9734"></script>
+
+    <script>
+        document.addEventListener("DOMContentLoaded", function () {
+            const queryParams = new URLSearchParams(window.location.search);
+            const utmParameters = {
+                utm_source: queryParams.get("utm_source"),
+                utm_medium: queryParams.get("utm_medium"),
+                utm_campaign: queryParams.get("utm_campaign"),
+                utm_term: queryParams.get("utm_term"),
+                utm_content: queryParams.get("utm_content"),
+            };
+            document.querySelectorAll("form").forEach((form) => {
+                Object.entries(utmParameters).forEach(([name, value]) => {
+                    if (value) { // Only append if value is not null
+                        const input = document.createElement("input");
+                        input.type = "hidden";
+                        input.name = name;
+                        input.value = value;
+                        form.appendChild(input);
+                    }
+                });
+            });
+        });
+    </script>
+    <link rel="stylesheet" href="http://localhost/primeraprueba/css/styles.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+
+    <script src="https://www.cvmabogados.com.pe/wp-content/themes/RUN4QCZ6C66jHxHAGqds/js/jquery.js"></script>
+    <script src="https://www.cvmabogados.com.pe/wp-content/themes/RUN4QCZ6C66jHxHAGqds/js/function.js?v=2.96"></script>
+    <script src="https://www.cvmabogados.com.pe/wp-content/themes/RUN4QCZ6C66jHxHAGqds/js/slick.js"></script>
+
+    <script src="https://www.cvmabogados.com.pe/wp-includes/js/wp-emoji-release.min.js?ver=6.6.2" defer></script>
+
+    <link rel="stylesheet" id="contact-form-7-css" href="https://www.cvmabogados.com.pe/wp-content/plugins/contact-form-7/includes/css/styles.css?ver=5.8" type="text/css" media="all">
+    <link rel="https://api.w.org/" href="https://www.cvmabogados.com.pe/wp-json/">
+    <link rel="EditURI" type="application/rsd+xml" title="RSD" href="https://www.cvmabogados.com.pe/xmlrpc.php?rsd">
+    <meta name="generator" content="WordPress 6.6.2">
+
+    <script>
+        site = {
+            isHome: "1",
+            baseUrl: "https://www.cvmabogados.com.pe",
+            BaseAjax: "https://www.cvmabogados.com.pe/wp-admin/admin-ajax.php",
+        }
+    </script>
+</head>
+
+<body>
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P94F9734"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+
+    <header>	
+        <div id="container_module_header">
+            <div class="center">
+                <div class="row_header">
+                    <div class="column_logo">
+                        <a href="http://localhost/primeraprueba/"><img src="images/logoDJP.png" alt="" style="width: 130px; height: auto;"></a>
+                    </div>
+                    <div class="column_menus">
+                        <div class="menu_principal">
+                            <div class="menu-menu-container">
+                            <ul id="menu-menu" class="menu">
+                                <li class="menu-item"><a href="http://localhost/primeraprueba/">Inicio</a></li>
+                                <li class="menu-item"><a href="http://localhost/primeraprueba/Nosotros.php">Nosotros</a></li>
+                                <li class="menu-item"><a href="http://localhost/primeraprueba/Servicios.php">Servicios</a></li>
+                                <li class="menu-item"><a href="http://localhost/primeraprueba/Equipo.php">Equipo</a></li>
+                                <li class="menu-item"><a href="http://localhost/primeraprueba/Contacto.php">Contacto</a></li>
+                            </ul>
+                            </div>						
+                        </div>
+                        <div class="row_search">
+                            <a href="http://localhost/primeraprueba/images/brochure_djp_2022.pdf" target="_blank"><i class="brochure"></i>BROCHURE</a>
+                        </div>
+                    </div>
+                    <div class="icon-menu">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
+                </div>
+            </div>
+        </div>
+	
+        <div id="menu-mobile">
+            <div class="menu_principals">
+                <div class="menu-menu-container">
+                    <ul id="menu-menu-1" class="menu">
+                        <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-96"><a href="http://localhost/primeraprueba/">Inicio</a></li>
+                        <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-97"><a href="http://localhost/primeraprueba/Nosotros.php">Nosotros</a></li>
+                        <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-98"><a href="http://localhost/primeraprueba/Servicios.php/">Servicios</a></li>
+                        <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-191"><a href="http://localhost/primeraprueba/Equipo.php">Equipo</a></li>
+                        <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-95"><a href="http://localhost/primeraprueba/Contacto.php">Contacto</a></li>
+                    </ul>		
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <main>
+        <div class="box-support">
+            <div class="support" style="display: block;">
+                <div class="button-support-close"></div>
+                <div class="title-support-close"></div>
+                <div class="link-support">
+                    <ul>
+                        <li>
+                            <a href="https://wa.me/51965080929?text=Hola, quiero información del servicio 1 de Deudas Judiciales Perú." data-phone="51965080929" data-phone-presentation="999995719" data-hrl="https://www.cvmabogados.com.pe/servicios/derecho-de-herencias-testamentos-y-sucesiones/" target="_blank">
+                                <div><img src="http://localhost/primeraprueba/images/logoDeud.png" alt=""></div>
+                                <div>
+                                    <h3>Servicio 1</h3>
+                                    <span>Disponible</span>
+                                </div>
+                            </a>
+                        </li>	
+                        <li>
+                            <a href="https://wa.me/51965080929?text=Hola, quiero información del servicio 2 de Deudas Judiciales Perú." data-phone="51957141671" data-phone-presentation="957141671" data-hrl="https://www.cvmabogados.com.pe/servicios/derecho-penal/" target="_blank">
+                                <div><img src="http://localhost/primeraprueba/images/logoDeud.png" alt=""></div>
+                                <div>
+                                    <h3>Servicio 2</h3>
+                                    <span>Disponible</span>
+                                </div>
+                            </a>
+                        </li>	
+                        <li>
+                            <a href="https://wa.me/51965080929?text=Hola, quiero información del servicio 3 de Deudas Judiciales Perú." data-phone="51910640039" data-phone-presentation="910640039" data-hrl="#" target="_blank">
+                                <div><img src="http://localhost/primeraprueba/images/logoDeud.png" alt=""></div>
+                                <div>
+                                    <h3>Servicio 3</h3>
+                                    <span>Disponible</span>
+                                </div>
+                            </a>
+                        </li>	
+                        <li>
+                            <a href="https://wa.me/51965080929?text=Hola, quiero información del servicio 4 de Deudas Judiciales Perú." data-phone="51927193307" data-phone-presentation="927193307" data-hrl="https://www.cvmabogados.com.pe/servicios/derecho-civil/" target="_blank">
+                                <div><img src="http://localhost/primeraprueba/images/logoDeud.png" alt=""></div>
+                                <div>
+                                    <h3>Servicio 4</h3>
+                                    <span>Disponible</span>
+                                </div>
+                            </a>
+                        </li>		
+                    </ul>
+                </div>
+            </div>
+            <div class="button-support">CONSULTAS URGENTES</div>
+        </div>
+        
+        <section>
+            <div id="container_module_slider">
+                <img src="http://localhost/primeraprueba/images/imginicio.jpg" alt="" class="imagenanimate">
+            </div>
+
+            <div id="container_module_us">
+                <div class="center">
+                    <div class="row_module_us">
+                        <div class="column_rightUs">
+                            <h2>NOSOTROS</h2>
+                            <h3>Tu aliado en la lucha legal</h3>
+                            <p>En <strong>Deudas Judiciales Perú</strong>, nos dedicamos a transformar tus desafíos legales en oportunidades. Nuestro compromiso con la excelencia y la innovación nos permite ofrecerte servicios de consultoría de alta calidad. Con un enfoque personalizado y el respaldo de modernas plataformas digitales, estamos aquí para guiarte en cada paso del proceso judicial. Tu tranquilidad es nuestra misión.</p>
+
+                            <div class="btn_us">	
+                                <a href="http://localhost/primeraprueba/Nosotros.php">CONOCE MÁS</a>
+                            </div>
+                        </div>
+                        <div class="column_leftUs">
+                            <img src="http://localhost/primeraprueba/images/imginicio.jpg" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+            <div id="container_module_services">
+                <div class="center">
+                    <div class="row_services">
+                        <h2>Servicios</h2>
+                        <span>Eficiencia garantizada</span>
+                    </div>
+                    <div class="group_services">
+                        <ul>
+                                
+                            <li>
+                                <div class="row_content_services">
+                                    <div class="services_ico">
+                                        <img src="https://www.cvmabogados.com.pe/wp-content/uploads/2023/09/ico_01.png" alt="">
+                                    </div>
+                                    <div class="content_image_services">
+                                                                        <img src="http://localhost/primeraprueba/images/servicio1.jpg" alt="">
+                                        <a href="http://localhost/primeraprueba/Servicios.php/">Consulta ahora</a>
+                                    </div>
+                                    <div class="services_content">
+                                        <h3>Demandas Judiciales</h3>
+                                        <p>"Defendemos tus derechos con pasión y experiencia, ¡tu libertad es nuestra prioridad absoluta en cada caso!"
+
+                                        </p>
+                                    </div>
+                                </div>
+                            </li>
+                                
+                            <li>
+                                <div class="row_content_services">
+                                    <div class="services_ico">
+                                        <img src="https://www.cvmabogados.com.pe/wp-content/uploads/2023/09/derechos-civiles-1.png" alt="">
+                                    </div>
+                                    <div class="content_image_services">
+                                                                        <img src="http://localhost/primeraprueba/images/servicio1.jpg" alt="">
+                                        <a href="http://localhost/primeraprueba/Servicios.php/">Consulta ahora</a>
+                                    </div>
+                                    <div class="services_content">
+                                        <h3>Derecho Civil</h3>
+                                        <p>"Resolvemos conflictos civiles con soluciones legales eficaces, protegiendo tus intereses y tu tranquilidad."</p>
+                                    </div>
+                                </div>
+                            </li>
+                                
+                            <li>
+                                <div class="row_content_services">
+                                    <div class="services_ico">
+                                        <img src="https://www.cvmabogados.com.pe/wp-content/uploads/2023/09/herencia-1.png" alt="">
+                                    </div>
+                                    <div class="content_image_services">
+                                                                        <img src="http://localhost/primeraprueba/images/servicio1.jpg" alt="">
+                                        <a href="http://localhost/primeraprueba/Servicios.php/">Consulta ahora</a>
+                                    </div>
+                                    <div class="services_content">
+                                        <h3>Derecho de Herencias, Testamentos y Sucesiones</h3>
+                                        <p>"Maximizamos tu legado familiar con estrategias expertas de sucesiones y protección patrimonial, ¡herencias seguras garantizadas!"</p>
+                                    </div>
+                                </div>
+                            </li>
+                                              
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+        </section>
+    </main>
+</body>
+
+</html>
